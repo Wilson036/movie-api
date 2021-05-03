a4a8718aa5ab584f38c557dc281b251e5986bba4
wilson kao <wilkao036@gmail.com>
2021-05-03 19:36:24 +0800

    更新redis

--- src/redis.ts
+++ src/redis.ts
@@ -1,12 +1,5 @@
 import Redis from 'ioredis';
-//const redisClient = process.env.REDIS_URI;
-// const redisConfig = {
-//   host: 'ec2-34-227-34-10.compute-1.amazonaws.com', // Redis host
-//   port: 21330, // Redis port
-//   password: 'p64a63871362ba54c2c0c7ac88d380c70d8d70d366de91b2d17f2efdf069a66e7',
-//   db: 4,
-//   connectTimeout: 10000,
-// };
-const connectionString =
-  'redis://:p64a63871362ba54c2c0c7ac88d380c70d8d70d366de91b2d17f2efdf069a66e7@ec2-34-227-34-10.compute-1.amazonaws.com:21130';
-export const redis = new Redis(connectionString);
+
+export const redis = new Redis(
+  'redis://:p64a63871362ba54c2c0c7ac88d380c70d8d70d366de91b2d17f2efdf069a66e7@ec2-34-227-34-10.compute-1.amazonaws.com:21129'
+);
